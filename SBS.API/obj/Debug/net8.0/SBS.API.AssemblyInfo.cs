ce5691e3401d8b1a104c2bcb4d97cd755ab49031
paradon[iCube] <paradon.tht@icube.co.th>
2026-03-19 20:21:12 +0700

    build: Refresh build artifacts and update assembly informational versions across projects.

--- SBS.API/obj/Debug/net8.0/SBS.API.AssemblyInfo.cs
+++ SBS.API/obj/Debug/net8.0/SBS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SBS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49aba6bbead90aeba8278f7d41ebcc95fd95074")]
 [assembly: System.Reflection.AssemblyProductAttribute("SBS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SBS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
